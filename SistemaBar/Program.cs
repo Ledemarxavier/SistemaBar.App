fec777c2bdef59caec1f4f710a95a0397eac436a
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-07-04 16:04:31 -0300

    Add project files.

---
+++ SistemaBar/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaBar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
