6ca61af250d4c7e13af96f7aae1b0721c2aeb027
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-08-03 20:37:11 -0300

    Implementado CRUD de Produto

---
+++ SistemaBar.WebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaBar.App.Dominio.ModuloMesa;
+using SistemaBar.App.Dominio.ModuloProduto;
+using SistemaBar.App.Infraestrutura.Arquivos.Compartilhado;
+using SistemaBar.App.Infraestrutura.Arquivos.ModuloProduto;
+using SistemaBar.WebApp.Models;
+
+namespace SistemaBar.WebApp.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly RepositorioProdutoEmArquivo repositorioProduto;
+
+        public ProdutoController()
+        {
+            ContextoDados contexto = new ContextoDados(carregarDados: true);
+
+            repositorioProduto = new RepositorioProdutoEmArquivo(contexto);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            CadastrarProdutoViewModel cadastrarVm = new CadastrarProdutoViewModel();
+
+            return View(cadastrarVm);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(CadastrarProdutoViewModel cadastrarVm)
+        {
+            if (!ModelState.IsValid)
+                return View(cadastrarVm);
+
+            var entidade = new Produto(cadastrarVm.Nome, cadastrarVm.Valor);
+
+            repositorioProduto.CadastrarRegistro(entidade);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Produto> produtos = repositorioProduto.SelecionarRegistros();
+
+            VisualizarProdutosViewModel visualizarVm = new VisualizarProdutosViewModel(produtos);
+
+            return View(visualizarVm);
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var registro = repositorioProduto.SelecionarRegistroPorId(id);
+
+            EditarProdutoViewModel editarVm = new EditarProdutoViewModel(
+                id,
+                registro.Nome,
+                registro.Valor
+            );
+
+            return View(editarVm);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EditarProdutoViewModel editarVm)
+        {
+            if (!ModelState.IsValid)
+                return View(editarVm);
+
+            var produtoEditado = new Produto(editarVm.Nome, editarVm.Valor);
+
+            repositorioProduto.EditarRegistro(editarVm.Id, produtoEditado);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var registro = repositorioProduto.SelecionarRegistroPorId(id);
+
+            ExcluirProdutoViewModel excluirVm = new ExcluirProdutoViewModel(
+                id,
+                registro.Nome
+            );
+
+            return View(excluirVm);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(ExcluirProdutoViewModel excluirVm)
+        {
+            repositorioProduto.ExcluirRegistro(excluirVm.Id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- SistemaBar.WebApp/Models/ProdutoViewModels.cs
+++ SistemaBar.WebApp/Models/ProdutoViewModels.cs
@@ -1,13 +1,67 @@
 ﻿using SistemaBar.App.Dominio.ModuloGarcom;
 using SistemaBar.App.Dominio.ModuloProduto;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaBar.WebApp.Models
 {
-    public class VizualizarProdutosViewModel
+    public class CadastrarProdutoViewModel
+    {
+        [Required(ErrorMessage = "O campo \"Nome\" é obrigatório.")]
+        [MinLength(2, ErrorMessage = "O campo \"Nome\" deve conter ao menos 2 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo \"Valor\" é obrigatório.")]
+        public decimal Valor { get; set; }
+
+        public CadastrarProdutoViewModel()
+        {
+        }
+    }
+
+    public class EditarProdutoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo \"Nome\" é obrigatório.")]
+        [MinLength(2, ErrorMessage = "O campo \"Nome\" deve conter ao menos 2 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo \"Valor\" é obrigatório.")]
+        public decimal Valor { get; set; }
+
+        public EditarProdutoViewModel()
+        {
+        }
+
+        public EditarProdutoViewModel(int id, string nome, decimal valor)
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+
+    public class ExcluirProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public ExcluirProdutoViewModel()
+        {
+        }
+
+        public ExcluirProdutoViewModel(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+
+    public class VisualizarProdutosViewModel
     {
         public List<DetalhesProdutoViewModel> Registros { get; set; } = new List<DetalhesProdutoViewModel>();
 
-        public VizualizarProdutosViewModel(List<Produto> produtos)
+        public VisualizarProdutosViewModel(List<Produto> produtos)
         {
             foreach (Produto m in produtos)
             {
