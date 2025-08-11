748c2e84f8af705f9898d20f0df8aac522823cf2
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-08-10 21:47:29 -0300

    Implementadas features de Gestão de Pedidos e Fechamento de Contas

--- SistemaBar.WebApp/Controllers/ProdutoController.cs
+++ SistemaBar.WebApp/Controllers/ProdutoController.cs
@@ -1,100 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SistemaBar.App.Dominio.ModuloMesa;
+﻿using SistemaBar.App.Infraestrutura.Arquivos.ModuloProduto;
+
+using Microsoft.AspNetCore.Mvc;
 using SistemaBar.App.Dominio.ModuloProduto;
 using SistemaBar.App.Infraestrutura.Arquivos.Compartilhado;
-using SistemaBar.App.Infraestrutura.Arquivos.ModuloProduto;
 using SistemaBar.WebApp.Models;
 
-namespace SistemaBar.WebApp.Controllers
+namespace SistemaBar.WebApp.Controllers;
+
+public class ProdutoController : Controller
 {
-    public class ProdutoController : Controller
+    private readonly RepositorioProdutoEmArquivo repositorioProduto;
+
+    public ProdutoController()
     {
-        private readonly RepositorioProdutoEmArquivo repositorioProduto;
+        ContextoDados contextoDados = new ContextoDados(carregarDados: true);
+        repositorioProduto = new RepositorioProdutoEmArquivo(contextoDados);
+    }
 
-        public ProdutoController()
-        {
-            ContextoDados contexto = new ContextoDados(carregarDados: true);
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<Produto> produtos = repositorioProduto.SelecionarRegistros();
 
-            repositorioProduto = new RepositorioProdutoEmArquivo(contexto);
-        }
+        VisualizarProdutosViewModel visualizarVm = new VisualizarProdutosViewModel(produtos);
 
-        [HttpGet]
-        public IActionResult Cadastrar()
-        {
-            CadastrarProdutoViewModel cadastrarVm = new CadastrarProdutoViewModel();
+        return View(visualizarVm);
+    }
 
+    [HttpGet]
+    public IActionResult Cadastrar()
+    {
+        CadastrarProdutoViewModel cadastrarVm = new CadastrarProdutoViewModel();
+
+        return View(cadastrarVm);
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar(CadastrarProdutoViewModel cadastrarVm)
+    {
+        if (!ModelState.IsValid)
             return View(cadastrarVm);
-        }
 
-        [HttpPost]
-        public IActionResult Cadastrar(CadastrarProdutoViewModel cadastrarVm)
-        {
-            if (!ModelState.IsValid)
-                return View(cadastrarVm);
+        var entidade = new Produto(cadastrarVm.Nome, cadastrarVm.Valor);
 
-            var entidade = new Produto(cadastrarVm.Nome, cadastrarVm.Valor);
+        repositorioProduto.CadastrarRegistro(entidade);
 
-            repositorioProduto.CadastrarRegistro(entidade);
+        return RedirectToAction(nameof(Index));
+    }
 
-            return RedirectToAction(nameof(Index));
-        }
+    [HttpGet]
+    public IActionResult Editar(int id)
+    {
+        var registro = repositorioProduto.SelecionarRegistroPorId(id);
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            List<Produto> produtos = repositorioProduto.SelecionarRegistros();
+        EditarProdutoViewModel editarVm = new EditarProdutoViewModel(
+            id,
+            registro.Nome,
+            registro.Valor
+        );
 
-            VisualizarProdutosViewModel visualizarVm = new VisualizarProdutosViewModel(produtos);
+        return View(editarVm);
+    }
 
-            return View(visualizarVm);
-        }
-
-        [HttpGet]
-        public IActionResult Editar(int id)
-        {
-            var registro = repositorioProduto.SelecionarRegistroPorId(id);
-
-            EditarProdutoViewModel editarVm = new EditarProdutoViewModel(
-                id,
-                registro.Nome,
-                registro.Valor
-            );
-
+    [HttpPost]
+    public IActionResult Editar(EditarProdutoViewModel editarVm)
+    {
+        if (!ModelState.IsValid)
             return View(editarVm);
-        }
 
-        [HttpPost]
-        public IActionResult Editar(EditarProdutoViewModel editarVm)
-        {
-            if (!ModelState.IsValid)
-                return View(editarVm);
+        var garcomEditado = new Produto(editarVm.Nome, editarVm.Valor);
 
-            var produtoEditado = new Produto(editarVm.Nome, editarVm.Valor);
+        repositorioProduto.EditarRegistro(editarVm.Id, garcomEditado);
 
-            repositorioProduto.EditarRegistro(editarVm.Id, produtoEditado);
+        return RedirectToAction(nameof(Index));
+    }
 
-            return RedirectToAction(nameof(Index));
-        }
+    [HttpGet]
+    public IActionResult Excluir(int id)
+    {
+        var registro = repositorioProduto.SelecionarRegistroPorId(id);
 
-        [HttpGet]
-        public IActionResult Excluir(int id)
-        {
-            var registro = repositorioProduto.SelecionarRegistroPorId(id);
+        ExcluirProdutoViewModel excluirVm = new ExcluirProdutoViewModel(
+            id,
+            registro.Nome
+        );
 
-            ExcluirProdutoViewModel excluirVm = new ExcluirProdutoViewModel(
-                id,
-                registro.Nome
-            );
+        return View(excluirVm);
+    }
 
-            return View(excluirVm);
-        }
+    [HttpPost]
+    public IActionResult Excluir(ExcluirProdutoViewModel excluirVm)
+    {
+        repositorioProduto.ExcluirRegistro(excluirVm.Id);
 
-        [HttpPost]
-        public IActionResult Excluir(ExcluirProdutoViewModel excluirVm)
-        {
-            repositorioProduto.ExcluirRegistro(excluirVm.Id);
-
-            return RedirectToAction(nameof(Index));
-        }
+        return RedirectToAction(nameof(Index));
     }
 }
